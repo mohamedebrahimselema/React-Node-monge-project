79413e97cd0088b375ef353222630faa07de5d1a
mohamedibrahemselema <mohamed.ibrahiem5905@ci.menofia.edu.eg>
2020-11-23 22:27:43 +0200

    add mobile navigation drawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,22 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
+import Backdrop from "../UIElements/Backdrop";
 import MainHeader from "./MainHeader";
+import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const openDrawer = () => {
+    return setIsDrawerOpen(true);
+  };
+  const closeDrawer = () => {
+    return setIsDrawerOpen(false);
+  };
   return (
-    <MainHeader>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">YourPlacs</Link>
-      </h1>
-      <nav>...</nav>
-    </MainHeader>
+    <React.Fragment>
+      {isDrawerOpen && <Backdrop onClick={closeDrawer} />}
+      {isDrawerOpen && (
+        <SideDrawer>
+          <nav className="main-navigation__drawer-nav">
+            <NavLinks />
+          </nav>
+        </SideDrawer>
+      )}
+      <MainHeader>
+        <button className="main-navigation__menu-btn" onClick={openDrawer}>
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">YourPlacs</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </MainHeader>
+    </React.Fragment>
   );
 };
 
